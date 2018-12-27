2689d110676286cc4150a2e860b569e65e8d4aae
cfhy <cfhy1076@sina.com>
2018-12-27 10:32:42 +0800

    Update Application.java
    取消@EnableTransactionManagement 注解

--- src/main/java/com/yyb/Application.java
+++ src/main/java/com/yyb/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @desc 建议放到根目录下面,主要用于做一些框架配置
  */
 @MapperScan("com.yyb.mapper")
-@EnableTransactionManagement 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
