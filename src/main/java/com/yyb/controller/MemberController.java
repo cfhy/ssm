e020ec000b5eda4a34751bb0146e007efe18ef53
yyb <yongbin@yang.medcrab.com>
2019-04-20 13:35:19 +0800

    会员维护完成

--- src/main/java/com/yyb/controller/MemberController.java
+++ src/main/java/com/yyb/controller/MemberController.java
@@ -1,28 +1,22 @@
 package com.yyb.controller;
 
 import com.yyb.model.Member;
-import com.yyb.model.ZhangWu;
 import com.yyb.service.MemberService;
-import com.yyb.service.ZhangWuService;
 import com.yyb.utils.ResponseData;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/member")
-public class MemberController extends BaseController {
+public class MemberController {
     private MemberService memberService = new MemberService();
 
 
-    @ResponseBody
     @RequestMapping(value = "login")
     public ResponseData commitLogin(String username, String password, HttpServletRequest request) {
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
@@ -33,9 +27,13 @@ public class MemberController extends BaseController {
         if (member == null) {
             return ResponseData.fail("用户名密码错误");
         }
-        if (request.getSession().getAttribute("member") == null) {
-            request.getSession().setAttribute("member", member);
-        }
+        request.getSession().setAttribute("member", member);
+        return ResponseData.success();
+    }
+
+    @RequestMapping(value = "logout")
+    public ResponseData logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("member");
         return ResponseData.success();
     }
 
@@ -44,12 +42,12 @@ public class MemberController extends BaseController {
      **/
     @RequestMapping(value = "/save")
     public ResponseData addMember(Member member) {
-        if(member.getId()!=null && member.getId()>0){
+        if (member.getId() != null && member.getId() > 0) {
             memberService.editMember(member);
-            return ResponseData.success("","修改会员成功");
-        }else {
+            return ResponseData.success("", "修改会员成功");
+        } else {
             memberService.addMember(member);
-            return ResponseData.success("","添加会员成功");
+            return ResponseData.success("", "添加会员成功");
         }
     }
 
@@ -59,7 +57,7 @@ public class MemberController extends BaseController {
     @RequestMapping(value = "/delete")
     public ResponseData deleteMember(int memberId) {
         memberService.deleteMember(memberId);
-        return ResponseData.success("","删除会员成功");
+        return ResponseData.success("", "删除会员成功");
     }
 
     /**
@@ -79,4 +77,19 @@ public class MemberController extends BaseController {
         return memberService.detail(memberId);
     }
 
+    /**
+     * 获取会员信息
+     *
+     * @return
+     */
+    @RequestMapping(value = "/getMemberInfo")
+    public Member getMemberInfo(HttpServletRequest request) {
+        Member member = null;
+        Object m = request.getSession().getAttribute("member");
+        if (m != null) {
+            member = (Member) m;
+        }
+        if (member == null) member = new Member();
+        return member;
+    }
 }
