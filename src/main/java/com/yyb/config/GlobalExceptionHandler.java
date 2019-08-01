ab2b48b2bad269263ff6e96e125567147b860a27
yyb <yongbin.yang@medcrab.com>
2019-08-01 09:50:21 +0800

    异常处理

--- src/main/java/com/yyb/config/GlobalExceptionHandler.java
+++ src/main/java/com/yyb/config/GlobalExceptionHandler.java
@@ -1,23 +1,11 @@
 package com.yyb.config;
 
 import com.yyb.common.response.ResponseData;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.UnexpectedRollbackException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 /**
  * Created by Administrator on 2017/11/7.
